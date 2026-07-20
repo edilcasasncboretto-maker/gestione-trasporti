38ff02ae26f3223580da76427ad58db178396ab2
edilcasasncboretto-maker <edilcasasnc.boretto@gmail.com>
2026-07-20 23:03:16 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
 import Calendario from './pages/Calendario'
-import NuovaConsegna from './pages/NuovaConsegna'
+import ConsegnaForm from './pages/ConsegnaForm'
 import Percorso from './pages/Percorso'
 import Scadenze from './pages/Scadenze'
+import Clienti from './pages/Clienti'
+import Restrizioni from './pages/Restrizioni'
 
 export default function App() {
   return (
@@ -13,9 +15,12 @@ export default function App() {
         <Route element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="calendario" element={<Calendario />} />
-          <Route path="nuova-consegna" element={<NuovaConsegna />} />
+          <Route path="nuova-consegna" element={<ConsegnaForm />} />
+          <Route path="modifica-consegna/:id" element={<ConsegnaForm />} />
           <Route path="percorso" element={<Percorso />} />
           <Route path="scadenze" element={<Scadenze />} />
+          <Route path="clienti" element={<Clienti />} />
+          <Route path="restrizioni" element={<Restrizioni />} />
         </Route>
       </Routes>
     </BrowserRouter>
